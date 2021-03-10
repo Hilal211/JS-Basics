4fb388a819397663df0d62aff45775d158757e53
hilal masri <hilal1922001@gmail.com>
2021-03-10 19:16:13 +0200

    step_6

---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,39 @@
+function change(id){
+    
+    if(id=="image1"){
+        document.querySelector("#image1").src="images/image1_2.jpg";
+    }
+else if(id=="image2"){
+    document.querySelector("#image2").src="images/image2_2.jpg";
+}
+else if(id=="image3"){
+    document.querySelector("#image3").src="images/image3_2.jpg";
+}
+else if(id=="image4"){
+    document.querySelector("#image4").src="images/image4_2.jpg";
+}
+else if(id=="image5"){
+    document.querySelector("#image5").src="images/image5_2.jpg";
+}
+
+}
+
+function change2(id){
+    
+    if(id=="image1"){
+        document.querySelector("#image1").src="images/image1.jpg";
+    }
+else if(id=="image2"){
+    document.querySelector("#image2").src="images/image2.jpg";
+}
+else if(id=="image3"){
+    document.querySelector("#image3").src="images/image3.jpg";
+}
+else if(id=="image4"){
+    document.querySelector("#image4").src="images/image4.jpg";
+}
+else if(id=="image5"){
+    document.querySelector("#image5").src="images/image5.jpg";
+}
+
+}
