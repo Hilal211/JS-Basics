cb4ab99f9013149c429ddfe889bd2e67df7fa67d
hilal masri <hilal1922001@gmail.com>
2021-03-10 19:20:21 +0200

    step_1

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,5 @@
+document.getElementById("image1").addEventListener("mouseover",change_style)
+function change_style(){
+    document.getElementById("image1").style.border="3px solid red";
+    
+    }
