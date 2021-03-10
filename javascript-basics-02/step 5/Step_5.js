b42276eb1671d4ce760d032fd7f1e5c7656327f9
hilal masri <hilal1922001@gmail.com>
2021-03-10 19:04:56 +0200

    step_5

---
+++ javascript-basics-02/step 5/Step_5.js
@@ -0,0 +1,19 @@
+function change(id){
+    
+    if(id=="image1"){
+        document.querySelector("#image1").src="images/image1_2.jpg";
+    }
+else if(id=="image2"){
+    document.querySelector("#image2").src="images/image2_2.jpg";
+}
+else if(id=="image3"){
+    document.querySelector("#image3").src="images/image3_2.jpg";
+}
+else if(id=="image4"){
+    document.querySelector("#image4").src="images/image4_2.jpg";
+}
+else if(id=="image5"){
+    document.querySelector("#image5").src="images/image5_2.jpg";
+}
+
+}
